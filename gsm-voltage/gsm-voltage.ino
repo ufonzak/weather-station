87e8fe40ce63ac03846baa2752c6223214985399
martin <martin@zaber.com>
2020-10-26 23:03:29 -0700

    Cleanup and averaging.

--- gsm-voltage/gsm-voltage.ino
+++ gsm-voltage/gsm-voltage.ino
@@ -1,6 +1,3 @@
-  
-String cmd;
-
 #define GSM_STATUS_PIN (12)
 #define GSM_POWER_PIN (5)
 #define VIN_VOLTAGE_PIN (A0)
@@ -8,23 +5,31 @@ String cmd;
 #define SOLAR_VOLTAGE_PIN (A2)
 #define VOLTAGE_REF_COEF (0.006472)
 #define VOLTAGE_SOLAR_COEF (0.01030)
+// #define GATEWAY "7785225231" 
+#define GATEWAY "2267814018"
 
 #define INFO_PERIOD (3600)
 int infoSeconds = 0;
 
-void turnOn() {
-  Serial.println("Powering On");
+bool turnOn() {
+  Serial.println("Powering On...");
   
-  while (true) {
-    bool isOn = digitalRead(GSM_STATUS_PIN) == HIGH;
-    if (isOn) {
-      break;
-    }
+  bool isOn = digitalRead(GSM_STATUS_PIN) == HIGH;
+  if (isOn) {
+    return true;
+  }
 
-    digitalWrite(GSM_POWER_PIN, LOW);
-    delay(2000);
-    digitalWrite(GSM_POWER_PIN, HIGH);
-    delay(2000);
+  digitalWrite(GSM_POWER_PIN, LOW);
+  delay(2000);
+  digitalWrite(GSM_POWER_PIN, HIGH);
+
+  for (int att = 5; att > 0 && !isOn; att--) {
+    delay(1000);
+    isOn = digitalRead(GSM_STATUS_PIN) == HIGH;
+  }
+  if (!isOn) {
+    Serial.println("Cannot turn on");
+    return false;
   }
 
   int seconds = 10;
@@ -33,21 +38,28 @@ void turnOn() {
   } while (Serial1.available() || --seconds > 0);
 
   Serial.println("ON");
+  return true;
 }
 
-void turnOff() {
-  Serial.println("Powering Off");
+bool turnOff() {
+  Serial.println("Powering Off...");
   
-  while (true) {
-    bool isOn = digitalRead(GSM_STATUS_PIN) == HIGH;
-    if (!isOn) {
-      break;
-    }
-
-    digitalWrite(GSM_POWER_PIN, LOW);
-    delay(2000);
-    digitalWrite(GSM_POWER_PIN, HIGH);
-    delay(2000);
+  bool isOn = digitalRead(GSM_STATUS_PIN) == HIGH;
+  if (!isOn) {
+    return true;
+  }
+  
+  digitalWrite(GSM_POWER_PIN, LOW);
+  delay(2000);
+  digitalWrite(GSM_POWER_PIN, HIGH);
+  
+  for (int att = 5; att > 0 && isOn; att--) {
+    delay(1000);
+    isOn = digitalRead(GSM_STATUS_PIN) == HIGH;
+  }
+  if (isOn) {
+    Serial.println("Cannot turn off");
+    return false;
   }
 
   do {
@@ -55,31 +67,53 @@ void turnOff() {
   } while (Serial1.available());
   
   Serial.println("OFF");
+  return true;
 }
+
+bool readReply(String& reply) {
+  reply = Serial1.readStringUntil('\n');
+  if (reply.compareTo("\r") != 0) {
+    Serial.print("Unexpected return (expected \\r): ");
+    Serial.println(reply);
+    return false;
+  }
+  
+  reply = Serial1.readStringUntil('\n');
+  if (reply.length() == 0) {
+    Serial.print("No reply");
+    return false;
+  }
+  
+  reply.trim();
+  return true;
+}
+
+
+bool readOk() {
+  String reply;
+  if (!readReply(reply)) {
+    return false;
+  }
+  return reply.compareTo("OK") == 0;
+}
+
 
 bool queryCmd(const char* toSend, String& reply) {
   Serial1.println(toSend);
   String line; 
-  
-  line = Serial1.readStringUntil('\n');
-  line.trim();
-  if (line.compareTo(toSend) != 0) {
-    Serial.print("Unexpected echo: ");
-    Serial.println(line);
+  if (!readReply(line)) {
     return false;
   }
-  
-  line = Serial1.readStringUntil('\n');
-  line.trim();
   int dataBegin = line.indexOf(':');
-  if (line.length() == 0 || line.compareTo("ERROR") == 0 || dataBegin < 0) {
-    Serial.print("Error: ");
+  if (dataBegin < 0) {
+    Serial.print("Error (query resp): ");
     Serial.println(reply);
     return false;
   }
   
   reply = line.substring(dataBegin + 1);
   reply.trim();
+  
   line = line.substring(0, dataBegin);
   String cmdPart(toSend + 2);
   if (!cmdPart.startsWith(line)) {
@@ -87,67 +121,24 @@ bool queryCmd(const char* toSend, String& reply) {
     Serial.println(line);
     return false;
   }
-  
-  line = Serial1.readStringUntil('\n');
-  if (line.compareTo("\r") != 0) {
-    Serial.print("Unexpected return: ");
-    Serial.println(line);
-    return false;
-  }
-  
-  line = Serial1.readStringUntil('\n');
-  line.trim();
-  if (line.compareTo("OK") != 0) {
-    Serial.print("NOK: ");
-    Serial.println(line);
-    return false;
-  }
-  return true;
+
+  return readOk();
 }
 
 bool setCmd(const char* toSend) {
   Serial1.println(toSend);
-  String line; 
-  
-  line = Serial1.readStringUntil('\n');
-  line.trim();
-  if (line.compareTo(toSend) != 0) {
-    Serial.print("Unexpected echo: ");
-    Serial.println(line);
-    return false;
-  }
-  
-  line = Serial1.readStringUntil('\n');
-  line.trim();
-  if (line.compareTo("OK") != 0) {
-    Serial.print("NOK: ");
-    Serial.println(line);
-    return false;
-  }
-  return true;
+  return readOk();
 }
 
-bool sendSms1(const char* number) {
-  if (!setCmd("AT+CMGF=1")) {
-    return false;
-  }
-  
+bool sendSms1(const char* number) {  
   Serial1.print("AT+CMGS=\"");
   Serial1.print(number);
   Serial1.println("\"");
-  
+
   String line; 
-  line = Serial1.readStringUntil('\n');
-  line.trim();
-  
-  if (!line.startsWith("AT+CMGS")) {
-    Serial.print("Unexpected echo: ");
-    Serial.println(line);
+  if (!readReply(line)) {
     return false;
   }
-
-  line = Serial1.readString();
-  line.trim();
   if (line.compareTo(">") != 0) {
     Serial.print("No prompt: ");
     Serial.println(line);
@@ -156,70 +147,85 @@ bool sendSms1(const char* number) {
   return true;
 }
 
-bool sendSms2() {
-  Serial1.println();
-  do {
-    Serial1.readString();
-    delay(100);
-  } while (Serial1.available());
-  
-  Serial1.write(26);
-  Serial1.flush();
-  Serial1.read();
-  
-  String line; 
-  int waitToSend = 10;
-  do {
-    line = Serial1.readStringUntil('\n');
-    line.trim();
-  } while (line.length() == 0 && --waitToSend > 0);
-  
-  if (!line.startsWith("+CMGS")) {
-    Serial.print("Unexpected reply (sms): ");
-    Serial.println(line);
-    return false;
+bool waitForData(int secondsToWait) {
+  while(!Serial1.available() && --secondsToWait > 0) {
+    delay(1000);
   }
-  
-  line = Serial1.readStringUntil('\n');
-  if (line.compareTo("\r") != 0) {
-    Serial.print("Unexpected return: ");
-    Serial.println(line);
-    return false;
-  }
-  
-  line = Serial1.readStringUntil('\n');
-  line.trim();
-  if (line.compareTo("OK") != 0) {
-    Serial.print("NOK: ");
-    Serial.println(line);
-    return false;
-  }
-  
-  return true;
+  return secondsToWait > 0;
 }
 
-bool waitForRegistration() {
-  while (true) {
-    if (!queryCmd("AT+CGREG?", cmd)) {
+bool sendSms2() {
+  Serial1.write(26);
+  Serial1.flush();
+  
+  if (!waitForData(15)) {
+    return false;
+  }
+  
+  String reply; 
+  if (!readReply(reply)) {
+    return false;
+  }
+  if (!reply.startsWith("+CMGS")) {
+    Serial.print("Unexpected reply (sms): ");
+    Serial.println(reply);
+    return false;
+  }  
+  
+  return readOk();
+}
+
+bool waitForRegistration(int secondsToWait) {
+  String reply;
+  for (int att = secondsToWait; att > 0; att--) {
+    if (!queryCmd("AT+CGREG?", reply)) {
       return false;
     }
     
-    if (cmd.compareTo("0,1") == 0) {
+    if (reply.compareTo("0,1") == 0) {
       Serial.println("On Network");
       return true;
     }
     
     Serial.print("Waiting for network: ");
-    Serial.println(cmd);
+    Serial.println(reply);
     delay(1000);
   }
+  return false;
+}
+
+unsigned long solarVoltageSum;
+unsigned long solarVoltageCount;
+
+float getSolarVoltageAvg() {
+  if (solarVoltageCount == 0) {
+    return 0;
+  }
+  return (solarVoltageSum / solarVoltageCount) * VOLTAGE_SOLAR_COEF;
+}
+
+void readSolarVoltage() {
+  int voltage = analogRead(SOLAR_VOLTAGE_PIN);
+  unsigned long oldSum = solarVoltageSum;
+  solarVoltageSum += (unsigned long)voltage;
+  if (oldSum > solarVoltageSum) {
+    Serial.println("Solar voltage sum overflow");
+    solarVoltageSum = voltage;
+    solarVoltageCount = 0;
+  }
+  solarVoltageCount++;
+}
+
+void resetSolarVoltage() {
+  solarVoltageSum = 0;
+  solarVoltageCount = 0;
 }
 
 bool sendInfo() {
-  if (!waitForRegistration()) {
+  if (!waitForRegistration(30)) {
     return false;
   }
-
+  
   String info;
   if (!queryCmd("AT+CBC", info)) {
     return false;
@@ -227,9 +233,9 @@ bool sendInfo() {
   
   float voltageVin = analogRead(VIN_VOLTAGE_PIN) * VOLTAGE_REF_COEF;
   float voltageBattery = analogRead(BAT_VOLTAGE_PIN) * VOLTAGE_REF_COEF;
-  float voltageSolar = analogRead(SOLAR_VOLTAGE_PIN) * VOLTAGE_SOLAR_COEF;
+  float voltageSolar = getSolarVoltageAvg();
   
-  if (!sendSms1("2267814018")) {
+  if (!sendSms1(GATEWAY)) {
     return false;
   }
   Serial1.print(info);
@@ -246,6 +252,55 @@ bool sendInfo() {
   return true;
 }
 
+void procesInput() {
+  if (!Serial.available()) {
+    return;
+  }
+  
+  String cmd = Serial.readStringUntil('\n');
+  if (cmd.startsWith("AT")) {
+    Serial1.println(cmd);
+
+    while(true) {
+      cmd = Serial1.readStringUntil('\n');
+      if (cmd.length() == 0){
+        break;
+      }
+      Serial.println(cmd);    
+    }
+  } else if (cmd.compareTo("test") == 0) {
+    turnOn();
+    if (!sendInfo()) {
+      Serial.println("sendInfo failed");    
+    }
+    turnOff();
+  } else if (cmd.compareTo("on") == 0) {
+    turnOn();
+  } else if (cmd.compareTo("off") == 0) {
+    turnOff();
+  } else if (cmd.compareTo("analog") == 0) {
+    float voltageVin = analogRead(VIN_VOLTAGE_PIN) * VOLTAGE_REF_COEF;
+    float voltageBattery = analogRead(BAT_VOLTAGE_PIN) * VOLTAGE_REF_COEF;
+    float voltageSolar = analogRead(SOLAR_VOLTAGE_PIN) * VOLTAGE_SOLAR_COEF;
+    Serial.print(voltageVin);
+    Serial.print(",");
+    Serial.print(voltageBattery);
+    Serial.print(",");
+    Serial.print(voltageSolar);
+    Serial.println();
+  } else if (cmd.compareTo("direct") == 0) {
+    while(true) {
+      if (Serial1.available()) {
+        cmd = Serial1.readString();
+        Serial.print(cmd);    
+      }
+      if (Serial.available()) {
+        cmd = Serial.readString();
+        Serial1.print(cmd);
+      }
+    }
+  }
+}
 
 void setup()
 {
@@ -261,59 +316,18 @@ void setup()
   Serial.begin(9600);
   Serial1.begin(9600);
 
-  // infoSeconds = INFO_PERIOD - 120;
+  infoSeconds = INFO_PERIOD - 120;
 }
 
-void loop() // run over and over
+void loop()
 {
-  if (Serial.available()) {
-    cmd = Serial.readStringUntil('\n');
-    if (cmd.startsWith("AT")) {
-      Serial1.println(cmd);
+  procesInput();
+  readSolarVoltage();
 
-      while(true) {
-        cmd = Serial1.readStringUntil('\n');
-        if (cmd.length() == 0){
-          break;
-        }
-        Serial.println(cmd);    
-      }
-    } else if (cmd.compareTo("test") == 0) {
-      turnOn();
-      if (!sendInfo()) {
-        Serial.println("sendInfo failed");    
-      }
-      turnOff();
-    } else if (cmd.compareTo("on") == 0) {
-      turnOn();
-    } else if (cmd.compareTo("off") == 0) {
-      turnOff();
-    } else if (cmd.compareTo("analog") == 0) {
-      float voltageVin = analogRead(VIN_VOLTAGE_PIN) * VOLTAGE_REF_COEF;
-      float voltageBattery = analogRead(BAT_VOLTAGE_PIN) * VOLTAGE_REF_COEF;
-      float voltageSolar = analogRead(SOLAR_VOLTAGE_PIN) * VOLTAGE_SOLAR_COEF;
-      Serial.print(voltageVin);
-      Serial.print(",");
-      Serial.print(voltageBattery);
-      Serial.print(",");
-      Serial.print(voltageSolar);
-      Serial.println();
-    } else if (cmd.compareTo("direct") == 0) {
-      while(true) {
-        if (Serial1.available()) {
-          cmd = Serial1.readString();
-          Serial.print(cmd);    
-        }
-        if (Serial.available()) {
-          cmd = Serial.readString();
-          Serial1.print(cmd);
-        }
-      }
-    }
-  }
-    
   digitalWrite(LED_BUILTIN, HIGH);
   delay(200);  
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(800);
 
   if (++infoSeconds >= INFO_PERIOD) {
     infoSeconds = 0;
@@ -321,8 +335,7 @@ void loop() // run over and over
     turnOn();
     sendInfo();
     turnOff();
-  }
-  
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(800);
+
+    resetSolarVoltage();
+  }  
 }
